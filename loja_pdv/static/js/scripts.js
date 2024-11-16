78b2f281ec7e7be2bf510c124a9641afda478e44
arusrex <arusbr@hotmail.com>
2024-11-16 17:31:19 -0300

    att

--- loja_pdv/static/js/scripts.js
+++ loja_pdv/static/js/scripts.js
@@ -10,3 +10,44 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+const dropDownToggler = document.querySelectorAll('.drop-toggler');
+
+dropDownToggler.forEach((toggler) => {
+    toggler.addEventListener('click', (event) => {
+        event.stopPropagation();
+
+        const togglerTarget = toggler.getAttribute('data-target');
+        const togglerById = document.getElementById(togglerTarget);
+
+        document.querySelectorAll('.drop-menu').forEach((menu) => {
+            if (menu !== togglerById) {
+                menu.classList.remove('show');
+            }
+        });
+
+        dropDownToggler.forEach((active) => {
+            if (active !== toggler) {
+                active.classList.remove('nav-item-active');
+            }
+        });
+
+        
+        if (togglerById) {
+            togglerById.classList.toggle('show');
+            toggler.classList.add('nav-item-active');
+        }
+    });
+});
+
+document.addEventListener('click', () => {
+    const allDrops = document.querySelectorAll('.drop-menu');
+    
+    allDrops.forEach((menu) => {
+        menu.classList.remove('show');
+    });
+
+    dropDownToggler.forEach((click) => {
+        click.classList.remove('nav-item-active');
+    });
+});
