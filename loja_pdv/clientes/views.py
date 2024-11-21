e636c8d60abb8317773329e09be7c709a6bb3213
arusrex <arusbr@hotmail.com>
2024-11-21 16:39:55 -0300

    att

--- loja_pdv/clientes/views.py
+++ loja_pdv/clientes/views.py
@@ -1,4 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
+from django.contrib import messages
+import logging
+
+log = logging.getLogger(__name__)
 
 def clientes(request):
-    return render(request, 'pages/clientes.html')
+    objs = PessoaFisica.objects.all().order_by('nome')
+    form = PessoaFisicaForm(request.POST or None)
+    tipo_cliente = request.POST.get('tipo-pessoa', '1')
+
+    if tipo_cliente == '2':
+        objs = PessoaJuridica.objects.all().order_by('razao_social')
+        form = PessoaJuridicaForm(request.POST or None)
+
+
+    if request.method == 'POST' and request.POST.get('btn-form') and form.is_valid():
+        form.save()
+        messages.success(request, 'Cliente registrado com sucesso')
+        log.info('Cliente registrado com sucesso')
+        return redirect('clientes:clientes')
+
+    context = {
+        'objs': objs,
+        'form': form,
+        'tipo_cliente': tipo_cliente,
+    }
+
+    return render(request, 'pages/clientes.html', context)
+
+
+def form_clientes(request, id=None, action=None):
+    ...
