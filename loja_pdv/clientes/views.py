24ddcd4019a3ecd3dcf766c7ec35b7939aee1986
arusrex <arusbr@hotmail.com>
2024-11-22 19:04:19 -0300

    at

--- loja_pdv/clientes/views.py
+++ loja_pdv/clientes/views.py
@@ -6,19 +6,6 @@ import logging
 
 log = logging.getLogger(__name__)
 
-# def clientes(request):
-#     tipo_cliente = request.GET.get('tipo-pessoa', '1')
-#     if tipo_cliente == '1':
-#         objs = PessoaFisica.objects.all().order_by('nome')
-#     else:
-#         objs = PessoaJuridica.objects.all().order_by('razao_social')
-
-#     context = {
-#         'objs': objs,
-#         'tipo_cliente': tipo_cliente,
-#     }
-
-#     return render(request, 'pages/clientes.html', context)
 
 def clientes(request):
     return form_clientes(request)
@@ -27,23 +14,31 @@ def edit_cliente(request, id, tipo_cliente):
     return form_clientes(request, id=id, tipo_cliente=tipo_cliente, is_edit=True)
 
 def form_clientes(request, id=None, tipo_cliente='1', is_edit=False):
+    cliente = None
+
     if not is_edit:
         tipo_cliente = request.GET.get('tipo-pessoa', '1')
     
     if tipo_cliente == '1':
         objs = PessoaFisica.objects.all().order_by('nome')
         form = PessoaFisicaForm(request.POST or None)
+        # obj = PessoaFisica()
+        # campos = [field.name for field in obj._meta.get_fields()] # type: ignore
         if is_edit:
             cliente = PessoaFisica.objects.get(id=id)
-            form = PessoaFisicaForm(request.POST, instance=cliente)
+            form = PessoaFisicaForm(request.POST or None, instance=cliente)
             print(f'edit fisico: {cliente}')
+
     elif tipo_cliente == '2':
         objs = PessoaJuridica.objects.all().order_by('razao_social')
         form = PessoaJuridicaForm(request.POST or None)
+        # obj = PessoaJuridica()
+        # campos = [field.name for field in obj._meta.get_fields()] # type: ignore
         if is_edit:
             cliente = PessoaJuridica.objects.get(id=id)
-            form = PessoaJuridicaForm(request.POST, instance=cliente)
+            form = PessoaJuridicaForm(request.POST or None, instance=cliente)
             print(f'edit juridico: {cliente}')
+
     else:
         return redirect('clientes:clientes')
 
@@ -55,19 +50,21 @@ def form_clientes(request, id=None, tipo_cliente='1', is_edit=False):
             log.info('Cliente editado com sucesso')
             return redirect('clientes:edit_user', id=cliente.id) # type: ignore
     
-    if request.method == 'POST' and request.POST.get('btn-form') and not is_edit:
+    if request.method == 'POST' and not is_edit:
         print('entrou no post new')
         if form.is_valid():
             form.save()
             messages.success(request, 'Cliente registrado com sucesso')
             log.info('Cliente registrado com sucesso')
             return redirect('clientes:clientes')
-        
+
     context = {
         'form': form,
         'objs': objs,
         'tipo_cliente': tipo_cliente,
         'is_edit': is_edit,
+        'cliente': cliente,
+        # 'campos': campos,
     }
 
     return render(request, 'pages/clientes.html', context)
