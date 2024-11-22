5e1875090f73a8947b484ee7e112cf5c57c21c97
arusrex <arusbr@hotmail.com>
2024-11-22 16:31:59 -0300

    att

--- loja_pdv/clientes/urls.py
+++ loja_pdv/clientes/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = 'clientes'
 
 urlpatterns = [
-    path('clientes/', clientes, name='clientes'),
+    path('gestao_clientes/', clientes, name='clientes'),
+    path('edit_cliente/<int:id>/<str:tipo_cliente>/', edit_cliente, name="edit_cliente"),
+    path('delete_cliente/<int:id>/<str:tipo_cliente>/', delete_cliente, name="delete_cliente"),
 ]
--- loja_pdv/clientes/views.py
+++ loja_pdv/clientes/views.py
@@ -6,30 +6,83 @@ import logging
 
 log = logging.getLogger(__name__)
 
-def clientes(request):
-    objs = PessoaFisica.objects.all().order_by('nome')
-    form = PessoaFisicaForm(request.POST or None)
-    tipo_cliente = request.POST.get('tipo-pessoa', '1')
+# def clientes(request):
+#     tipo_cliente = request.GET.get('tipo-pessoa', '1')
+#     if tipo_cliente == '1':
+#         objs = PessoaFisica.objects.all().order_by('nome')
+#     else:
+#         objs = PessoaJuridica.objects.all().order_by('razao_social')
 
-    if tipo_cliente == '2':
+#     context = {
+#         'objs': objs,
+#         'tipo_cliente': tipo_cliente,
+#     }
+
+#     return render(request, 'pages/clientes.html', context)
+
+def clientes(request):
+    return form_clientes(request)
+
+def edit_cliente(request, id, tipo_cliente):
+    return form_clientes(request, id=id, tipo_cliente=tipo_cliente, is_edit=True)
+
+def form_clientes(request, id=None, tipo_cliente='1', is_edit=False):
+    if not is_edit:
+        tipo_cliente = request.GET.get('tipo-pessoa', '1')
+    
+    if tipo_cliente == '1':
+        objs = PessoaFisica.objects.all().order_by('nome')
+        form = PessoaFisicaForm(request.POST or None)
+        if is_edit:
+            cliente = PessoaFisica.objects.get(id=id)
+            form = PessoaFisicaForm(request.POST, instance=cliente)
+            print(f'edit fisico: {cliente}')
+    elif tipo_cliente == '2':
         objs = PessoaJuridica.objects.all().order_by('razao_social')
         form = PessoaJuridicaForm(request.POST or None)
-
-
-    if request.method == 'POST' and request.POST.get('btn-form') and form.is_valid():
-        form.save()
-        messages.success(request, 'Cliente registrado com sucesso')
-        log.info('Cliente registrado com sucesso')
+        if is_edit:
+            cliente = PessoaJuridica.objects.get(id=id)
+            form = PessoaJuridicaForm(request.POST, instance=cliente)
+            print(f'edit juridico: {cliente}')
+    else:
         return redirect('clientes:clientes')
 
+    if request.method == 'POST' and request.POST.get('btn-form') and is_edit:
+        print(f'entrou no post edit: {cliente}')
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Cliente editado com sucesso')
+            log.info('Cliente editado com sucesso')
+            return redirect('clientes:edit_user', id=cliente.id) # type: ignore
+    
+    if request.method == 'POST' and request.POST.get('btn-form') and not is_edit:
+        print('entrou no post new')
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Cliente registrado com sucesso')
+            log.info('Cliente registrado com sucesso')
+            return redirect('clientes:clientes')
+        
     context = {
-        'objs': objs,
         'form': form,
+        'objs': objs,
         'tipo_cliente': tipo_cliente,
+        'is_edit': is_edit,
     }
 
     return render(request, 'pages/clientes.html', context)
 
-
-def form_clientes(request, id=None, action=None):
-    ...
+def delete_cliente(request, id):
+    tipo_cliente = request.GET.get('tipo_pessoa', '1')
+    cliente = PessoaFisica.objects.get(id=id)
+    if tipo_cliente == '2':
+        cliente = PessoaJuridica.objects.get(id=id)
+        cliente.delete()
+        messages.success(request, f'Cliente {cliente.nome_fantasia} excluído com sucesso')
+        log.info('Cliente pessoa jurídica excluída com sucesso')
+        return redirect('cliente:clientes')
+    else:
+        cliente.delete()
+        messages.success(request, f'Cliente {cliente.nome} excluído com sucesso')
+        log.info(f'Cliente {cliente.nome} excluído com sucesso')
+        return redirect('clientes:clientes')
