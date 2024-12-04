33f64720bf834ef962f04f1cbf3c4de38e513b7d
arusrex <arusbr@hotmail.com>
2024-12-04 15:31:07 -0300

    att

--- loja_pdv/vendas/urls.py
+++ loja_pdv/vendas/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('gestao_vendas/', vendas, name='vendas'),
     path('pdv/', pdv, name='pdv'),
     path('adicionar/<int:produto_id>/', adicionar, name='adicionar'),
-    # path('remover/<int:item_id>/', remover, name='remover'),
+    path('remover/<int:item_id>/', remover, name='remover'),
     # path('finalizar/', finalizar, name='finalizar'),
 ]
--- loja_pdv/vendas/views.py
+++ loja_pdv/vendas/views.py
@@ -9,9 +9,17 @@ def vendas(request):
     }
     return render(request, 'pages/vendas.html', context)
 
+def calcular_subtotal(quantidade, preco_unitario):
+    subtotal = quantidade * preco_unitario
+    return subtotal
+
 def pdv(request):
     venda = Venda.objects.filter(finalizada=False).first()
     produtos = Produto.objects.all().order_by('nome')
+
+    for item in venda.itens.all():
+        venda.total += item.subtotal
+
     context = {
         'venda': venda,
         'produtos': produtos,
@@ -20,30 +28,40 @@ def pdv(request):
 
 def adicionar(request, produto_id):
     produto = Produto.objects.get(id=produto_id)
-    venda, created = Venda.objects.get_or_create(
-        finalizada=False
-    )
+    venda = Venda.objects.filter(finalizada=False).first()
+
+    if not venda:
+        venda = Venda.objects.create(
+            finalizada=False
+        )
     
     if request.method == 'POST':
-        quantidade = request.POST.get('quantidade')
-        subtotal = int(quantidade) * produto.preco
-
-        item, item_created = ItemVenda.objects.get_or_create(
-            venda=venda,
-            produto=produto,
-            quantidade=quantidade,
-            subtotal=subtotal
-        )
-
-        if not item_created:
-            item.quantidade += 1
+        quantidade = int(request.POST.get('quantidade'))
+        item_venda = venda.itens.filter(produto=produto).first()
+        if item_venda:
+            item_venda.quantidade += quantidade
+            item_venda.subtotal = calcular_subtotal(item_venda.quantidade, produto.preco)
+            item_venda.save()
+        else:
+            item = ItemVenda.objects.create(
+                venda=venda,
+                produto=produto,
+                quantidade=quantidade,
+                subtotal=calcular_subtotal(quantidade, produto.preco)
+            )
+            
             item.save()
         
-        return redirect('vendas:pdv')
+    return redirect('vendas:pdv')
         
 
 def remover(request, item_id):
-    ...
+    venda = Venda.objects.filter(finalizada=False).first()
+    itens = ItemVenda.objects.filter(venda=venda)
+    for i in itens:
+        if i.id == item_id:
+            i.delete()
+    return redirect('vendas:pdv')
 
 def finalizar(request):
     ...
